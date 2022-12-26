8d76bbc69852d7c931c42b90ed9b6c67f2e20abc
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2022-12-26 06:09:17 +0530

    k

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import { useState } from "react";
 // import About from "./components/About";
 import NavBar from "./components/NavBar.js";
 import TextForm from "./components/TextForm.js";
-import Alert from "./components/Alert.js";
+// import Alert from "./components/Alert.js";
 
 function App() {
   const [mode, setDarkMode] = useState("light"); //whether dark mode is enable or not
 
-  const [alert, setAlert] = useState("light");
+  // <-------------Alert function -------------------->
 
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
-  };
+  // const [alert, setAlert] = useState("light");
+
+  // const showAlert = (message, type) => {
+  //   setAlert({
+  //     msg: message,
+  //     type: type,
+  //   });
+  // setTimeout(() => {
+  //   setAlert(null);
+  // }, 1500);
+  // };
 
   const toggleMode = () => {
     if (mode === "light") {
       setDarkMode("dark");
       document.body.style.backgroundColor = "#093145";
-      showAlert(" Dark mode is enabled", "success");
+      // showAlert(" Dark mode is enabled", "success");
     } else {
       setDarkMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert(" Light mode is enabled", "success");
+      // showAlert(" Light mode is enabled", "success");
     }
   };
   return (
@@ -46,7 +48,7 @@ function App() {
           <TextForm
             heading='Enter the Text to Analyze below'
             mode={mode}
-            showAlert={showAlert}
+            // showAlert={showAlert}
           />
         }
 
